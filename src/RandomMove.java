c8265dd05a89c5600a527fa6e433f48bda108acf
ssushi1 <quangtri.ha@students.mq.edu.au>
2023-08-28 13:43:20 +1000

    week 6

---
+++ src/RandomMove.java
@@ -0,0 +1,7 @@
+public class RandomMove implements moveStrat {
+    @Override
+    public Cell chooseLoc(list<Cell> possibleLocs) {
+        // Get a random index in possibleLocs
+        // return the cell at that index
+    }
+}
